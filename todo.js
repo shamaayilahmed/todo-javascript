ceb11c881aa62b30d68806cdeaacac1ae2886aad
shamaayil ahmed <shamayilahmad12@gmail.com>
2020-09-28 21:37:21 +0530

    added basic functionalities

--- todo.js
+++ todo.js
@@ -1,12 +1,25 @@
 const form=document.getElementById('form');
 const input=document.getElementById('input');
+const todos=document.getElementById('todos');
 
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
 
-  const todo=input.value;
+  const todoTxt=input.value;
 
-  if(todo){
-    
+  if(todoTxt){
+    const todoEl=document.createElement('li');
+    todoEl.innerText=todoTxt;
+
+    todoEl.addEventListener('click',()=>{
+      todoEl.classList.toggle('completed');
+    });
+
+    todoEl.addEventListener('contextmenu',(e)=>{
+      e.preventDefault();
+      todoEl.remove();
+    })
+    todos.appendChild(todoEl);
+    input.value='';
   }
 })
