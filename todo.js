36119cd5f8c52dd7e70ec0bf8839296c41b82d5f
shamaayil ahmed <shamayilahmad12@gmail.com>
2020-09-29 09:29:53 +0530

    completed the project with whole functionalities

--- todo.js
+++ todo.js
@@ -1,25 +1,63 @@
 const form=document.getElementById('form');
 const input=document.getElementById('input');
-const todos=document.getElementById('todos');
+const todosUl=document.getElementById('todos');
+
+const todos=JSON.parse(localStorage.getItem('todos'));
+
+if(todos){
+  todos.forEach(todo=>{
+    addTodo(todo);
+  })
+}
 
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
 
-  const todoTxt=input.value;
+  addTodo();
+})
 
+function addTodo(todo){
+
+  let todoTxt=input.value;
+  if(todo){
+    todoTxt=todo.text;
+  }
+  
   if(todoTxt){
     const todoEl=document.createElement('li');
     todoEl.innerText=todoTxt;
 
+    if(todo && todo.completed){
+      todoEl.classList.add('completed');
+    }
     todoEl.addEventListener('click',()=>{
       todoEl.classList.toggle('completed');
+
+      updateLS();
     });
 
     todoEl.addEventListener('contextmenu',(e)=>{
       e.preventDefault();
       todoEl.remove();
+
+      updateLS();
     })
-    todos.appendChild(todoEl);
+    todosUl.appendChild(todoEl);
     input.value='';
   }
-})
+
+  updateLS();
+}
+
+function updateLS(){
+  const todoEl=document.querySelectorAll("li");
+  const todos=[];
+  todoEl.forEach((todoEl)=>{
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem('todos',JSON.stringify(todos));
+}
